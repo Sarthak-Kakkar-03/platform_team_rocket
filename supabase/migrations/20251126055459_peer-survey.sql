c09ac10f19540867e15479e51b9155290b47468e
Koyo Hashi <126726684+koyohashi@users.noreply.github.com>
2025-12-03 18:33:36 -0500

    Simplifying migration into two

--- supabase/migrations/20251126055459_peer-survey.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS peer_surveys (
-    target_public_profile_id UUID NOT NULL REFERENCES user_roles(public_profile_id),
-    survey_response_id UUID NOT NULL REFERENCES survey_responses(id)
-);
-
-
-
--- supabase/migrations/20251126102030_add_survey_type.sql
+++
@@ -1,2 +0,0 @@
-CREATE TYPE survey_type AS ENUM ('assign_all', 'specific_students', 'peer_review');
-ALTER TABLE IF EXISTS surveys ADD COLUMN IF NOT EXISTS survey_type survey_type NOT NULL DEFAULT 'assign_all';
